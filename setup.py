900f032eff447083fffd2a191f781b2f85412434
DiegoGAVB <diego.fan@gavb.com.br>
2021-06-16 17:10:46 -0300

    Adicionando o script no seup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
+# Read requirements.txt and transform in a list.
+with open('requirements.txt', 'r') as fp:
+    reqs = fp.read().split('\n')[:-1]
+
+
 setup(
     name='mlutils',
     packages=find_packages(include=['mlutils']),
     version='0.1.0',
     description='My first Python library test',
     author='GAVB',
-    install_requires=[],
+    install_requires=reqs,
     setup_requires=['pytest_runner'],
     tests_require=['pytest== 6.2.4'],
     tests_suite='tests',
