6c490a3794beec507f3671e57d2d8472ab1b72f8
DiegoGAVB <diego.fan@gavb.com.br>
2021-09-28 09:49:48 -0300

    Alterando movie_files_e_create_dir

--- 8_movie_files_e_create_dir.py
+++ 8_movie_files_e_create_dir.py
@@ -1,16 +1,18 @@
 import shutil
 import os
+from pathlib import Path
 
 
 def move_files(source_dir: str, dest_dir: str):
     """
     moves the files from source_dir to dest_dir.
+    
     :param source_dir:  
     :type: Str
     :param dest_dir:
     :type: Str
-    :return:  
-    :rtype:
+    :return:    
+    :rtype: str
 
     """
     create_dir(dest_dir)
@@ -20,16 +22,20 @@ def move_files(source_dir: str, dest_dir: str):
         
 
 
-#TODO: Fazer a função ficar invariante a SO.
+
 def create_dir(save_dir: str): 
     """
     Creates directoy if doesn't exist.
     :param path
     :type: str
+    :raise FileExistsError: File already exists
     :return: 
     :rtype: str  
    
     """
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    try:
+        Path(save_dir).mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        print("Folder already exists")
+    else:
+        print("Folder was created")
