0028afa604b9bdf76542d64163979e97f638a683
lrcorre10 <91264911+lrcorre10@users.noreply.github.com>
2021-10-05 15:17:37 -0300

    ajuste do pytest diego

--- tests/test_weighted_mean_absolute_percentage_error.py
+++ tests/test_weighted_mean_absolute_percentage_error.py
@@ -1,22 +1,15 @@
-import pytest
 import numpy as np
 import pandas as pd
 from mlutils.weighted_mean_absolute_percentage_error import * 
 
 
+def test_wmap():
 
-def get_df(m, n):
-    y = pd.DataFrame(np.random.rand(m,n))
-    return y
-
-def test_wmap(y_true, y_pred):
+    y_true = pd.DataFrame(np.random.rand(6,5))
+    y_pred = pd.DataFrame(np.random.rand(6,5))
 
     resultado = weighted_mean_absolute_percentage_error(y_true, y_pred)
-    return resultado
+    
+    return print(resultado)
 
-
-y_true = get_df(6,5)
-y_pred = get_df(6,5)
-
-
-print(test_wmap(y_true, y_pred))
+test_wmap()
