4e78189397936ded770618a26f986c67d14e7fad
Diego Fan <diegoeco.fan@gmail.com>
2021-12-01 09:57:04 -0300

    Teste do pytest e feita algumas mudanças

--- tests/test_movie_files_create_dir.py
+++ tests/test_movie_files_create_dir.py
@@ -13,8 +13,6 @@ def test_move_file():
     with open(test_file, "w") as fd:
         fd.write("Testando para ver se move")
     move_files(str(dirpath), str(dest_dir))
-    # import pdb
-    # pdb.set_trace()
     assert (dest_dir / "test.txt").is_file()
 
 
