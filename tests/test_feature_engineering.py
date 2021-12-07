34551bd1556e33432bfe72e5c23c3430eb8674aa
lrcorre10 <91264911+lrcorre10@users.noreply.github.com>
2021-12-07 15:26:18 -0300

    new upgrades

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -82,7 +82,7 @@ def test_feature_engineering_feature_selection_f_regression():
     ]
 
 
-def test_feature_engineering_feature_selection_mutual_information(): # Resultado variando
+def test_feature_engineering_feature_selection_mutual_information():
 
     boston_data = datasets.load_boston()
     df_boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
@@ -92,69 +92,4 @@ def test_feature_engineering_feature_selection_mutual_information(): # Resultado
         "INDUS",
         "RM",
         "LSTAT",
-    ]
-
-
-def test_feature_engineering_select_data_error_to_process():
-    with pytest.raises(Exception) as ex:
-        boston_data = datasets.load_boston()
-        df_boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
-        df_boston["target"] = boston_data.target
-    
-        select_data(df_boston, "tar")
-        assert ex.message == "an error occured during DataFrame handling"
-
-
-def test_feature_engineering_feature_selection_filter_error_to_process():
-    with pytest.raises(Exception) as ex:
-        iris_data = datasets.load_iris()
-        df_iris = pd.DataFrame(data=iris_data.data, columns=iris_data.feature_names)
-        df_iris["target"] = iris_data.target
-
-        feature_selection_filter(df=df, target="tar", num_feats=3)
-        assert ex.message == "An error occured during filter selection process"
-        
-
-def test_feature_engineering_feature_selection_wrapper_error_to_process():
-    with pytest.raises(Exception) as ex:
-        iris_data = datasets.load_iris()
-        df_iris = pd.DataFrame(data=iris_data.data, columns=iris_data.feature_names)
-        df_iris["target"] = iris_data.target
-        feature_selection_wrapper(df_iris, "tar", 3)
-        assert ex.message == "An error occured during wrapper selection process"
-
-def test_feature_engineering_feature_selection_embedded_error_to_process():
-    with pytest.raises(Exception) as ex:
-        iris_data = datasets.load_iris()
-        df_iris = pd.DataFrame(data=iris_data.data, columns=iris_data.feature_names)
-        df_iris["target"] = iris_data.target
-
-        feature_selection_embedded(df_iris, "tar", 3, 50)
-        assert ex.message == "An error occured during embedded selection process"
-
-def test_feature_engineering_feature_selection_stepwise_error_to_process():
-    with pytest.raises(Exception) as ex:
-        boston_data = datasets.load_boston()
-        df_boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
-        df_boston["target"] = boston_data.target
-
-        feature_selection_stepwise(df_boston, "tar", 0.01, 0.05, False)
-        assert ex.message == "An error occured during step wise selection process"
-
-def test_feature_engineering_feature_selection_f_regression_error_to_process():
-    with pytest.raises(Exception) as ex:
-        boston_data = datasets.load_boston()
-        df_boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
-        df_boston["target"] = boston_data.target
-
-        feature_selection_f_regression(df_boston, "tar", 3)   
-        assert ex.message == "An error occured during f_regression selection process"
-
-def test_feature_engineering_feature_selection_mutual_information_error_to_process(): # Resultado variando
-    with pytest.raises(Exception) as ex:
-        boston_data = datasets.load_boston()
-        df_boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
-        df_boston["target"] = boston_data.target
-
-        feature_selection_mutual_information(df_boston, "target", 3)
-        assert ex.message == "An error occured during mutual_info_regression selection process"    
+    ]    
