6514f7a0304c46ba4c4f6624792841d88bdfd63f
Diego Fan <diegoeco.fan@gmail.com>
2021-12-13 15:00:48 -0300

    Changing the lib

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -1,4 +1,6 @@
 from numpy.testing._private.utils import assert_almost_equal
+from mlutils.variable_treat import *
+import numpy as np
 from sklearn import datasets
 import pandas as pd
 from mlutils.feature_engineering import *
@@ -93,3 +95,34 @@ def test_feature_engineering_feature_selection_mutual_information():
         "RM",
         "LSTAT",
     ]
+
+
+
+def test_vt():
+
+    df2 = pd.DataFrame(
+        np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=["a", "b", "c"]
+    )
+
+    assert variable_treat(
+        df2, "a", lower=True, lower_percentile=1.0, upper=True, upper_percentile=2.0
+    ) == [0, 1, 2]
+
+
+test_vt()
+
+
+def test_passes():
+    with pytest.raises(NotImplementedError) as ex:
+        df2 = pd.DataFrame(
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=["a", "b", "c"]
+        )
+        variable_treat(
+            df2,
+            "a",
+            lower=False,
+            lower_percentile=1.0,
+            upper=False,
+            upper_percentile=90.0,
+        )
+        assert ex.message == "One of those paramns must be True"
