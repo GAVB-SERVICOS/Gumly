6926d33d13f738340e23634b445de7d41fe60a76
Diego Fan <diegoeco.fan@gmail.com>
2022-01-27 17:29:38 -0300

    starting deploy

--- tests/test_value_validation.py
+++ tests/test_value_validation.py
@@ -1,4 +1,6 @@
-from mlutils.value_validation import assert_check_number, check_number
+from mlutils.value_validation import assert_check_number, check_number, check_dtypes
+from sklearn import datasets
+import pandas as pd
 
 import pytest
 
@@ -26,3 +28,17 @@ def test_argument_validation_error():
         lower_percentil = -1
 
         assert_check_number(lower_percentil, 0, 1.0, "lower_percentil")
+
+def test_check_dtypes_error():
+    
+    x = pd.DataFrame({'a': [1,2,3], 'b': ['1','2','3']})
+    y = pd.DataFrame({'a': [1,2,3], 'b': [1.0,1.2,3.0]})
+    
+    with pytest.raises(ValueError):
+
+        check_dtypes(x, ['str'])
+    
+    check_dtypes(y, ['str'])
+    
+    
+    
