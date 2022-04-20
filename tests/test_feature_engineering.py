71160acf4fa068fe273af9a1ec6a6c6981c66455
Henrique Branco <henrique.alves@grupoboticario.com.br>
2022-04-20 14:24:31 -0300

    feat: drop_columns_with_null_threshold implemented with test

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -104,3 +104,29 @@ def test_of():
         0,
         2,
     ]
+
+def test_drop_columns_with_null_threshold():
+
+    df_input = pd.DataFrame(
+        [
+            ('Jack', np.NaN, 34, 'Sydney', np.NaN, 5, np.NaN, np.NaN, np.NaN),
+            ('Riti', 23, 31, 'Delhi', np.NaN, 7, np.NaN, np.NaN, np.NaN),
+            ('Aadi', np.NaN, 16, 'London', 32, 11, np.NaN, 3, np.NaN),
+            ('Mark', np.NaN, 41, 'Delhi', np.NaN, 12, np.NaN, 11, 1),
+        ],
+        columns=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    )
+
+    df_out = drop_columns_with_null_threshold(df_input, 0.75)
+
+    df_expected = pd.DataFrame(
+        [
+            ('Jack', 34, 'Sydney', 5, np.NaN),
+            ('Riti', 31, 'Delhi', 7, np.NaN),
+            ('Aadi', 16, 'London', 11, 3),
+            ('Mark', 41, 'Delhi', 12, 11),
+        ],
+        columns=['A', 'C', 'D', 'F', 'H'],
+    )
+
+    pd.testing.assert_frame_equal(df_out, df_expected)
