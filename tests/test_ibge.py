f30565bf86da5e28096a8ab956c26f71ecfac5d0
Diego Fan <diegoeco.fan@gmail.com>
2022-06-22 17:33:08 -0300

    Made tests

--- tests/test_ibge.py
+++ tests/test_ibge.py
@@ -1,5 +1,5 @@
 # From the module
-from gumly.ibge import fetch
+from gumly.ibge import city_to_imediate_region, city_to_intermediarie_region, fetch
 from gumly.ibge import city_to_region
 from gumly.ibge import city_to_microregion
 from gumly.ibge import city_to_mesoregion
@@ -24,8 +24,140 @@ def test_city_to_region():
 
     ibge_data = {'municipios_regiao': {'saopaulo': 'Sudeste'}}
 
-    espected = pd.Series(['Sudeste', 'Sudeste', 'Sudeste', np.nan], name='temp')
+    expected = pd.Series(['Sudeste', 'Sudeste', 'Sudeste', np.nan], name='temp')
 
     result = city_to_region(df, 'City', ibge_data)
 
-    assert_series_equal(result, espected, check_dtype=False, check_categorical=False)
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+
+def test_city_to_microregion():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'municipios_microrregiao': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = city_to_microregion(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+
+def test_city_to_mesoregion():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'municipios_mesorregiao': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = city_to_mesoregion(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+def test_city_to_imediate_region():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'municipios_regiao_imediata': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = city_to_imediate_region(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+
+def test_city_to_intermediarie_region():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'municipios_regiao_intermediaria': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = city_to_intermediarie_region(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+def test_city_to_state():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'municipios_estado': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = city_to_state(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+
+def test_state_to_region():
+    d = {
+        'Customer': [1, 2, 3, 4],
+        'City': ['São Paulo', 'Sao Paulo', 'sao paulo', 'São Pauol'],
+    }  # With a typo on the last entry
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'regiao': {'saopaulo': 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo', np.nan], name='temp')
+
+    result = state_to_region(df, 'City', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+def test_cep_to_state():
+    d = {
+        'Customer': [1, 2, 3],
+        'CEP' : ['03033-070', '03033070', '03033']
+    } 
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'cep_estado':{pd.Interval(1000000, 19999999, closed='both'): 'São Paulo',pd.Interval(1000, 19999, closed='both'): 'São Paulo'}}
+
+    expected = pd.Series(['São Paulo', 'São Paulo', 'São Paulo'], name='temp')
+
+    result = cep_to_state(df, 'CEP', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
+
+def test_cep_to_region():
+    d = {
+        'Customer': [1, 2, 3],
+        'CEP' : ['03033-070', '03033070', '03033']
+    } 
+
+    df = pd.DataFrame(data=d)
+
+    ibge_data = {'regiao':{pd.Interval(1000000, 19999999, closed='both'): 'São Paulo',pd.Interval(1000, 19999, closed='both'): 'São Paulo'}}
+
+    expected = pd.Series(['Sudeste', 'Sudeste', 'Sudeste'], name='temp')
+
+    result = cep_to_region(df, 'CEP', ibge_data)
+
+    assert_series_equal(result, expected, check_dtype=False, check_categorical=False)
