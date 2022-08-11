ad9bd6ef6fd0da415b87fc36b26672748c82514b
Diego Fan <diegoeco.fan@gmail.com>
2022-08-11 16:15:08 -0300

    Finishing checkpoint_flow tests

--- tests/test_checkpoint_flow.py
+++ tests/test_checkpoint_flow.py
@@ -61,6 +61,7 @@ def test_checkpointflow_with_error(fixture_checkpointflow):
     state['c'] = 4.5
   with pytest.raises(StepException) as ex:
     cp.run(initial_state) 
+    assert str(ex) == "This thing broke!"
   cp_state = cp.handler.read()
   
   #Fixing f2 function
@@ -73,23 +74,33 @@ def test_checkpointflow_with_error(fixture_checkpointflow):
   assert cp_state['checkpoint_index'] == 0
 
 
+def test_checkpointflow_with_params(fixture_checkpointflow):
+  cp, initial_state = fixture_checkpointflow
+  def loader3(state):
+    print("loader3")
+    return {'z': ['A', 'B', 'C']}
 
+  def saver3(state, data):
+    print(f"saver3 printing data={data}")
   
+  @cp.add_step()
+  def f2(state, data):
+    print(f"f2 printing state: {state}")
+    print(f"f2 printing data: {data}")
+    state['b'] = 5.5
 
+  @cp.add_step(load=loader3, save=saver3)
+  def f3(state, data):
+    print(f"f3 printing state: {state}")
+    print(f"f3 printing data: {data}")
+    state['c'] = 4.5
 
-
-
-# with pytest.raises(AssertionError) as ex:                
-    
-
-#       @cp.add_step()
-#       def f2(state, data):
-#         error=Exception("This thing broke!")
-#         return error
-      
-#       @cp.add_step(load=loader3, save=saver3)
-#       def f3(state, data):
-#         print(f"f3 printing state: {state}")
-#         print(f"f3 printing data: {data}")
-#         state['c'] = 4.5 
+  cp.run(initial_state, checkpoint=1)
+  assert initial_state['b'] == 5.5
+  cp.run(initial_state, load_policy='always')
+  cp_state = cp.handler.read()
+  cp.handler.write(cp_state)
+  cp.run(initial_state, load_policy='always')
+  cp_state = cp.handler.read()
+  assert cp_state['d']== 'D'
 
